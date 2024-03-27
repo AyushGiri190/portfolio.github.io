a8ba389439690cb684cffd756ea6de14ad09b91a
AyushGiri190 <96994260+AyushGiri190@users.noreply.github.com>
2024-03-27 19:52:26 +0530

    Add files via upload

---
+++ send-email.php
@@ -0,0 +1,29 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    
+    // Recipient Email Address
+    $to = "your-email@example.com";
+    
+    // Subject
+    $subject = "New Contact Message from $name";
+    
+    // Email content
+    $email_content = "Name: $name\n";
+    $email_content .= "Email: $email\n\n";
+    $email_content .= "Message:\n$message\n";
+    
+    // Email headers
+    $headers = "From: $name <$email>\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    
+    // Send email
+    if (mail($to, $subject, $email_content, $headers)) {
+        echo "Your message has been sent successfully!";
+    } else {
+        echo "Oops! Something went wrong. Please try again later.";
+    }
+}
+?>
